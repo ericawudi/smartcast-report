6970df3d66fe0bcbe44483455b9e7481ce9de32e
Awudi Eric Okyere <info@eoawudi.com>
2022-12-05 05:36:32 +0000

    added background image

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,26 +4,29 @@ import LoginForm from "../components/loginform";
 
 function Login(props) {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justify="center"
-      style={{ minHeight: "100vh" }}
-    >
-      <Grid item xs={12} sm={6}>
-        <Paper
-          elevation={2}
-          style={{
-            padding: "10%",
-            paddingTop: "5%",
-          }}
-        >
-          <LoginForm {...props} />
-        </Paper>
+    <div className="login">
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={12} sm={6}>
+          <Paper
+            elevation={2}
+            style={{
+              padding: "10%",
+              paddingTop: "5%",
+              opacity: "0.95",
+            }}
+          >
+            <LoginForm {...props} />
+          </Paper>
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 }
 
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -4,26 +4,28 @@ import Signupform from "../components/signupform";
 
 function Signup(props) {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justify="center"
-      style={{ minHeight: "100vh" }}
-    >
-      <Grid item xs={12} sm={6}>
-        <Paper
-          elevation={2}
-          style={{
-            padding: "10%",
-            paddingTop: "5%",
-          }}
-        >
-          <Signupform {...props} />
-        </Paper>
+    <div className="signup">
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={12} sm={6}>
+          <Paper
+            elevation={2}
+            style={{
+              padding: "10%",
+              paddingTop: "5%",
+            }}
+          >
+            <Signupform {...props} />
+          </Paper>
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 }
 
